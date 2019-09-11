7506819b808b4bb7e896ac4c48290ffe2403b2f3
David B. Waters <mrdavidbwaters@gmail.com>
2019-09-11 18:35:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ *  Prettier Config
+ */
+
+module.exports = {
+  'printWidth': 60,
+  'tabWidth': 2,
+  'useTabs': false,
+  'semi': false,
+  'singleQuote': true,
+  'quoteProps': 'consistent',
+  'jsxSingleQuote': false,
+  'trailingComma': '',
+  'brackeSpacing': true,
+  'jsxBracketSameLine': false,
+  'arrowParens': 'avoid',
+  'rangeStart': 0,
+  'rangeEnd': 0,
+  'parser': '',
+  'filePath': '',
+  'requirePragma': false,
+  'insertPragma': false,
+  'proseWrap': 'preserve',
+  'htmlWhitespaceSensitivity': 'css',
+  'endOfLine': 'auto'
+}
